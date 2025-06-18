d896b9d8ef6c52f0d8610963db5d2a791ad42a87
Nikil <snikilpaul@gmail.com>
2025-06-18 23:41:22 +0530

    initial commit

---
+++ multi_tool_agent/firebase_client.py
@@ -0,0 +1,8 @@
+import firebase_admin
+from firebase_admin import credentials, firestore
+
+def get_firestore_client():
+    if not firebase_admin._apps:
+        cred = credentials.Certificate("firestore.json")
+        firebase_admin.initialize_app(cred)
+    return firestore.client()
